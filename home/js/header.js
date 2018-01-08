9b5c3218b068ada91e8d44fb862041edfe0a09bc
YuuNaa <413752366@qq.com>
2018-01-08 10:54:00 +0800

    four integration

--- home/js/header.js
+++ home/js/header.js
@@ -17,7 +17,7 @@ $(".herf").on({
         if (Targu == '9块9包邮') window.parent.frames.location.href = "../修修/nine.html";
         if (Targu == '排行榜') window.parent.frames.location.href = "../修修/paihangbang.html";
         if (Targu == '品牌特卖') window.parent.frames.location.href = "../王亚坤/temai/temai.html";
-        if (Targu == '发现好物') window.parent.frames.location.href = "../半糖首页/好物.html";
+        if (Targu == '发现好物') window.parent.frames.location.href = "../王亚坤/faxian/faxian.html";
         if (Targu == '手机版') window.parent.frames.location.href = "../王亚坤/phone/手机版.html";
     }
 });
